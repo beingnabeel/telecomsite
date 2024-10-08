22d816cbf0acbfd41d00de2ae0b4353bbbf473a6
Nabeel Hassan <sk.hassan2013@gmail.com>
2024-10-09 00:58:17 +0530

    mute local feed

--- telecom-front-end/src/videoComponents/VideoButton/VideoButton.js
+++ telecom-front-end/src/videoComponents/VideoButton/VideoButton.js
@@ -1,20 +1,34 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import startLocalVideoStream from "./startLocalVideoStream";
+import updateCallStatus from "../../redux-elements/actions/updateCallStatus";
 
 const VideoButton = ({ smallFeedEl }) => {
   const callStatus = useSelector((state) => state.callStatus);
   const streams = useSelector((state) => state.streams);
   const [pendingUpdate, setPendingUpdate] = useState(false);
+  const dispatch = useDispatch();
   const startStopVideo = () => {
     // console.log("Sanity check");
     // first check if the video is enabled, if so disabled
-    // second, check if the video is disabled, if so enable.
-    // thirdly, check to see if we have media, if so, start the stream.
-    console.log(callStatus.haveMedia);
-    if (callStatus.haveMedia) {
+    if (callStatus.video === "enabled") {
+      // update redux callStatus
+      dispatch(updateCallStatus("video", "disabled"));
+      //   set the stream to disabled
+      const tracks = streams.localStream.stream.getVideoTracks();
+      tracks.forEach((t) => (t.enabled = false));
+    } else if (callStatus.video === "disabled") {
+      // second, check if the video is disabled, if so enable.
+      dispatch(updateCallStatus("video", "enabled"));
+      const tracks = streams.localStream.stream.getVideoTracks();
+      tracks.forEach((t) => (t.enabled = true));
+      // console.log(callStatus.haveMedia);
+    } else if (callStatus.haveMedia) {
+      // thirdly, check to see if we have media, if so, start the stream.
       // we have the media! show the feed.
       smallFeedEl.current.srcObject = streams.localStream.stream;
       //   add tracks to the existing peerConnection.
+      startLocalVideoStream(streams, dispatch);
     } else {
       // lastly, it is possible, we don't have the media, wait for the media then start the stream.
       setPendingUpdate(true);
@@ -26,6 +40,8 @@ const VideoButton = ({ smallFeedEl }) => {
       console.log("Pending update succeeded.");
       setPendingUpdate(false); // switch back to false
       smallFeedEl.current.srcObject = streams.localStream.stream;
+      startLocalVideoStream(streams, dispatch);
+      //   we did it becoz we were waiting for the media to exist and it does exist now
     }
   }, [pendingUpdate, callStatus.haveMedia]);
   return (
@@ -34,7 +50,7 @@ const VideoButton = ({ smallFeedEl }) => {
       <div className="button camera" onClick={startStopVideo}>
         <i className="fa fa-video"></i>
         <div className="btn-text">
-          {callStatus.video === "display" ? "Stop" : "Start"} Video
+          {callStatus.video === "enabled" ? "Stop" : "Start"} Video
         </div>
       </div>
     </div>
--- telecom-front-end/src/videoComponents/VideoButton/startLocalVideoStream.js
+++ telecom-front-end/src/videoComponents/VideoButton/startLocalVideoStream.js
@@ -1,4 +1,25 @@
 // this function job is to update all PeerConnection (addTracks) and update redux callStatus
+// here the reason we are needing streams is because we are gonna interact with the peerConnection and we need dispatch because we need to update redux callstatus.
 
-const startLocalVideoStream = () => {};
+import updateCallStatus from "../../redux-elements/actions/updateCallStatus";
+
+const startLocalVideoStream = (streams, dispatch) => {
+  //   console.log("sanity check");
+  const localStream = streams.localStream;
+  for (const s in streams) {
+    // s is the key, and remember key is the who property, ex: remote1, remote2, local1
+    if (s !== "localStream") {
+      // we don't addTracks to the localStream.
+      const curStream = streams[s];
+      //   addTracks to all peerConnection.
+      localStream.stream.getVideoTracks().forEach((t) => {
+        curStream.peerConnection.addTrack(t, curStream.stream);
+      });
+      //   update redux callstatus.
+      //   here the update call status is part of redux it's not react.
+      //   dispatch needs to be inside component but the updateCallStatus doesn't need to be inside the component.
+      dispatch(updateCallStatus('video', 'enabled'));
+    }
+  }
+};
 export default startLocalVideoStream;
